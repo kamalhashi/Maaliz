1ba2fb4ba84f4a80a2549fcd50aa1e5b9fcd09f3
Kamal Weheliye <kamalweheliye@gmail.com>
2016-07-30 14:20:15 +0300

    ok

--- src/main/resources/static/js/ng-slick/slick-carousel/slick/slick.js
+++ src/main/resources/static/js/ng-slick/slick-carousel/slick/slick.js
@@ -69,7 +69,32 @@
                 pauseOnFocus: true,
                 pauseOnDotsHover: false,
                 respondTo: 'window',
-                responsive: null,
+                responsive: [
+                             {
+                               breakpoint: 1024,
+                               settings: {
+                                 slidesToShow: 4,
+                                 slidesToScroll: 1
+                               }
+                             },
+                             {
+                               breakpoint: 600,
+                               settings: {
+                                 slidesToShow: 3,
+                                 slidesToScroll: 1
+                               }
+                             },
+                             {
+                               breakpoint: 320,
+                               settings: {
+                                 slidesToShow: 2,
+                                 slidesToScroll: 1
+                               }
+                             }
+                             // You can unslick at a given breakpoint now by adding:
+                             // settings: "unslick"
+                             // instead of a settings object
+                           ],
                 rows: 1,
                 rtl: false,
                 slide: '',
