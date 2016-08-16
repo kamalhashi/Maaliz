1f3f1f0488cf9ee1e89ad24c6058500d7527c428
Kamal Weheliye <kamalweheliye@gmail.com>
2016-08-16 15:15:49 +0300

    ok

--- src/test/java/xaashi/CategoriesRepositoryTest.java
+++ src/test/java/xaashi/CategoriesRepositoryTest.java
@@ -2,21 +2,6 @@ package xaashi;
 
 
 import java.io.IOException;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import org.jclouds.s3.domain.AccessControlList.EmailAddressGrantee;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,23 +10,12 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.hashi.UiApplication;
-import com.hashi.repository.UserRepository;
-import com.hashi.rest.domain.User;
-import com.hashi.rest.domain.UserRole;
-import com.hashi.rest.domain.VerificationToken;
-import com.hashi.rest.enums.MailType;
-import com.hashi.rest.enums.Role;
-import com.hashi.rest.service.EmailService;
-import com.hashi.rest.service.ProfileProductService;
-import com.hashi.rest.vo.EmailAdPendingVO;
 
-
-
+/*
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = UiApplication.class)
 @WebAppConfiguration
-
+*/
 public class CategoriesRepositoryTest {
 /*
 	@Autowired
@@ -57,7 +31,7 @@ public class CategoriesRepositoryTest {
 	
 	@Autowired
 	UserRepository userRepository;
-	*/
+	
 	@Autowired
 	ProfileProductService profileProductService;
 	
@@ -66,11 +40,11 @@ public class CategoriesRepositoryTest {
 	@Autowired
 	private UserRepository userRepository;
 
-
+*/
 	@Test
 	public void testEmail(){
-	    EmailAdPendingVO emailAdPending = new EmailAdPendingVO();
-		emailService.sendEmailAdPending(userRepository.findOne(822L));
+	    //EmailAdPendingVO emailAdPending = new EmailAdPendingVO();
+		//emailService.sendEmailAdPending(userRepository.findOne(822L));
 	}
 	public void testName() throws IOException {
 		/*
@@ -85,7 +59,7 @@ public class CategoriesRepositoryTest {
 		for (int i=0; i<list.size(); i++){
 			Object[] row = (Object[]) list.get(i);
 			System.out.println("Element "+i+Arrays.toString(row));
-		}*/
+		}
 		
 		//productService.deleteProduct(306L);
 		
@@ -118,7 +92,7 @@ public class CategoriesRepositoryTest {
 		
 		//Product p = new Product();
 		
-		System.out.println("Done:");
+		System.out.println("Done:");*/
 	}		
 }
 
